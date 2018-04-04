f26d003ef7ea11f38dd98698fb8cc49cfcef8b5d
John Roberts <jfr10@leicester.ac.uk>
2018-04-04 15:14:45 +0100

    Continuing with sen2cor

--- pyeo/Sen2Land.py
+++ pyeo/Sen2Land.py
@@ -3,6 +3,8 @@ import logging
 
 
 def sen2cor_process(safe_filepath, conf):
-    sen2cor_cmd = 'L2A_Process --resolution 10 {}'.format(safe_filepath)
+    #On windows
+    sen2cor_path = conf["post_processing"]["sen2cor_path"]
+    sen2cor_cmd = 'L2A_Process.bat --resolution 10 {}'.format(safe_filepath)
     logging.DEBUG(sen2cor_cmd)
     os.system()
