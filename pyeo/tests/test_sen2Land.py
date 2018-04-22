ef05af9dffe4fe0c37cc3ed9d2cac5729d87109c
John Roberts <jfr10@leicester.ac.uk>
2018-04-22 20:22:07 -0300

    Sen2Cor test failing on windows due to a windows thing.
    Don't worry, this is actually going to plan.

--- pyeo/tests/test_sen2Land.py
+++ pyeo/tests/test_sen2Land.py
@@ -1,3 +1,11 @@
 import Sen2Land
+import configparser
+import os
 
-
+def test_sen2cor_process_windows():
+    #Tests that Sen2Cor throws an error message
+    conf = configparser.ConfigParser()
+    conf.read(os.path.join("..", "conf.ini"))
+    target = "made/up/filepath"
+    out = Sen2Land.sen2cor_process_windows(target, conf)
+    assert False
