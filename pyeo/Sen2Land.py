8cfc47d239de3905d92df954a6e7fb8901cc9012
John Roberts <jfr10@leicester.ac.uk>
2018-04-22 21:34:22 -0300

    Sen2Cor fails 'right' now; but still needs refinement

--- pyeo/Sen2Land.py
+++ pyeo/Sen2Land.py
@@ -3,20 +3,25 @@ import logging
 import platform
 import subprocess
 
+
 def sen2cor_process(safe_filepath, conf):
     """Uses sen2cor to calculate surface reflectance."""
     if platform.system() == "Windows":
         sen2cor_process_windows(safe_filepath, conf)
 
 
-
-
 def sen2cor_process_windows(safe_filepath, conf):
     sen2cor_path = conf["post_processing"]["sen2cor_path"]
-    sen2cor_cmd = 'L2A_Process.bat {}'.format(safe_filepath)
-    sen2cor_out = subprocess.run([sen2cor_path, safe_filepath], stdout=subprocess.PIPE,
-                                 stderr=subprocess.PIPE)
-    return sen2cor_out
+    sen2cor_cmd = os.path.join(sen2cor_path, 'L2A_Process.bat {}'.format(safe_filepath))
+    proc = subprocess.Popen(sen2cor_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    # There's got to be a better way to get the live out/stderr than this.
+    while not proc.poll:
+        print(proc.stdout.read())
+        print(proc.stderr.read())
+    print(proc.stdout.read())
+    print(proc.stderr.read())
+    return proc.returncode
+
 
 def sen2cor_process_unix(safe_filepath, conf):
     # TODO Implement
