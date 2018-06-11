b4aa5e347273f2249f05f2bba841fc94cd901965
Ciaran1981 <ciaran.robb@gmail.com>
2018-06-11 11:04:22 +0100

    Porting sen2cor_process_unix from ForestSentinel

--- pyeo/Sen2Land.py
+++ pyeo/Sen2Land.py
@@ -2,6 +2,8 @@ import os
 import logging
 import platform
 import subprocess
+import glob
+import re
 
 
 def sen2cor_process(safe_filepath, conf):
@@ -23,6 +25,49 @@ def sen2cor_process_windows(safe_filepath, conf):
     return proc.returncode
 
 
-def sen2cor_process_unix(safe_filepath, conf):
-    # TODO Implement
-    pass
+def sen2cor_process_unix(image_directory: str, out_directory: str, L2A_path: str,
+                           delete_unprocessed_image: bool=False):
+    """Applies Sen2cor cloud correction to level 1C images."""
+    log = logging.getLogger(__name__)
+    images = [image for image in os.listdir(image_directory)
+              if image.startswith('MSIL1C', 4)]
+    log.info(images)
+    for image in images:
+        image_path = os.path.join(image_directory, image)
+        image_timestamp = get_sen_2_image_timestamp(image)
+        if glob.glob(os.path.join(out_directory, r"*_{}_*".format(image_timestamp))):
+            log.warning("{} exists. Skipping.".format(image))
+            continue
+        # Here be OS magic. Since sen2cor runs in its own process, Python has to spin around and wait
+        # for it; since it's doing that, it may as well be logging the output from sen2cor. This
+        # approatch can be multithreaded in future to process multiple image (1 per core) but that
+        # will take some work to make sure they all finish before the program moves on.
+        sen2cor_proc = subprocess.Popen([L2A_path, image_path],
+                                        stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                                        universal_newlines=True)
+        try:
+            while True:
+                nextline = sen2cor_proc.stdout.readline()
+                if nextline == '' and sen2cor_proc.poll() is not None:
+                    break
+                if "CRITICAL" in nextline:
+                    log.error(nextline)
+                    break
+                log.info(nextline)
+        except subprocess.CalledProcessError:
+            log.error("Sen2Cor failed")
+            break
+        log.info("sen2cor processing finished for {}".format(image_path))
+        if delete_unprocessed_image:
+            os.rmdir(image_path)
+    l2_glob = os.path.join(image_directory, r"*_MSIL2A_*")
+    for l2_path in glob.glob(l2_glob):
+        l2_name = os.path.basename(l2_path)
+        os.rename(l2_path, os.path.join(out_directory, l2_name))
+
+
+def get_sen_2_image_timestamp(image_name: str):
+    """Returns the timestamps part of a Sentinel 2 image"""
+    timestamp_re = r"\d{8}T\d{6}"
+    ts_result = re.search(timestamp_re, image_name)
+    return ts_result.group(0)
