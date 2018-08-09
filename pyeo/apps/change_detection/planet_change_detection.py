b9c12daf126ee610981c0620379d49b04cd0b46a
John Roberts <jfr10@le.ac.uk>
2018-08-09 10:54:38 +0100

    Creating file and blocking out processing chain

---
+++ pyeo/apps/change_detection/planet_change_detection.py
@@ -0,0 +1,18 @@
+import core as pyeo
+
+# Load config
+
+# Download two images
+
+# Save RGB
+
+# Do classification
+
+# Do the imshow transitive closure trick
+
+# Polygonise
+
+# Burn
+
+# Resample
+
