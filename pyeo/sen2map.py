cbd99e53d02cadb3ef0e81284a897a701d167a76
hb91 <hb91@le.ac.uk>
2018-08-10 12:15:42 +0100

    sen2map.py Identified the issue with the error on distributed node when getting projection from shapefile. Needs fixing. Trying to set CCRS via Proj4 implemented skipping of existing tiff directories - this works now.

--- pyeo/sen2map.py
+++ pyeo/sen2map.py
@@ -1244,16 +1244,16 @@ def map_it(rgbdata, tifproj, mapextent, imgextent, shapefile, plotfile='map.jpg'
 
     # get the projection information and convert to wkt
     projosr = layer.GetSpatialRef()
+    proj4 = projosr.exportToProj4()
+    shapeproj = ccrs(proj4)
     #projwkt = projsr.ExportToWkt()
     #projosr = osr.SpatialReference()
     #projosr.ImportFromWkt(projwkt)
     # convert wkt projection to Cartopy projection
-    projcs = projosr.GetAuthorityCode('PROJCS')
-
-    print("EPSG projection:")
-    print(projcs)
-
-    shapeproj = ccrs.epsg(projcs)
+    #projcs = projosr.GetAttrValue('AUTHORITY')
+    #projcs = projosr.GetAuthorityCode('PROJCS')
+    #projcs = projosr.GetAuthorityCode(None)
+    #shapeproj = ccrs.epsg(projcs)
 
     # END OF TODO
 
