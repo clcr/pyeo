7b8f7767ad06d1d49a9063cb7d07b3e2598c917a
Polyanna Da Conceicao Bispo <pdcb1@alice03.cm.cluster>
2018-09-10 16:43:36 +0100

    Fixing init bug in cmfr

--- pyeo/apps/model_creation/create_model_from_region.py
+++ pyeo/apps/model_creation/create_model_from_region.py
@@ -8,16 +8,15 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Produces a trained model from a filder containing '
                                                  'rasters and shapefiles')
-    parser.add_argument("region_path", dest='region_path', type=str, action='store',
+    parser.add_argument("region_path", type=str, action='store',
                         help="Path to the folder containing the region data")
-    parser.add_argument("training_field", dest="training_class", type=str, action='store',
+    parser.add_argument("training_class", type=str, action='store',
                         help="Attribute field holding the class to train on")
-    parser.add_argument("out_path", dest="out_path", type=str, action="store",
+    parser.add_argument("out_path", type=str, action="store",
                         help="Path for the output .pkl file")
     args = parser.parse_args()
 
     conf = configparser.ConfigParser()
-    conf.read(args.config_path)
 
     log = pyeo.init_log(".")
 
