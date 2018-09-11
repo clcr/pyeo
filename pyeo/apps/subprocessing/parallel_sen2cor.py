5b2085e705bb2128b096295f469e7958502fc847
John R <jfr10@le.ac.uk>
2018-09-11 22:46:24 +0100

    Maybe this is working now

--- pyeo/apps/subprocessing/parallel_sen2cor.py
+++ pyeo/apps/subprocessing/parallel_sen2cor.py
@@ -2,16 +2,19 @@ import sys, os
 sys.path.insert(0, os.path.abspath(os.path.join(__file__, '..', '..', '..', '..')))
 import pyeo.core
 import argparse
+import shutil
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Parallel sen2cor')
-    parser.add_argument('l1_path', action='store', help="Path to the directory containing L1 imagery")
-    parser.add_argument('l2_path', action='store', help="Path to directory to contain L2 imagery")
-    array_id = os.getenv("PBS_ARRAYID")
+    parser.add_argument('l1_dir', action='store', help="Path to the directory containing L1 imagery")
+    parser.add_argument('l2_dir', action='store', help="Path to directory to contain L2 imagery")
+    array_id = int(os.getenv("PBS_ARRAYID"))
     args = parser.parse_args()
 
     pyeo.core.init_log("sen2cor_{}.log".format(array_id))
 
-    file_list = os.path.join(args.l1_path, os.listdir(args.l1_path).sort())
+    file_list = [os.path.join(args.l1_dir, l1_filename) for l1_filename in sorted(os.listdir(args.l1_dir))]
 
-    pyeo.core.apply_sen2cor(file_list[array_id], args.l2_path)
+    l2_name = pyeo.core.apply_sen2cor(file_list[array_id], r"/scratch/clcr/shared/Sen2Cor-02.05.05-Linux64/bin/L2A_Process")
+    shutil.move(os.path.join(args.l1_dir, l2_name), args.l2_dir)
+
