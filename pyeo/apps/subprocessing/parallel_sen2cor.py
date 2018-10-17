7aa76a5f513f12239a491b042e8b85c87f4bc66b
John Roberts <jfr10@le.ac.uk>
2018-10-17 14:15:28 +0100

    Updated parallel_sen2cor to handle multiple instances

--- pyeo/apps/subprocessing/parallel_sen2cor.py
+++ pyeo/apps/subprocessing/parallel_sen2cor.py
@@ -9,9 +9,15 @@ if __name__ == "__main__":
     parser.add_argument('l1_dir', action='store', help="Path to the directory containing L1 imagery")
     parser.add_argument('l2_dir', action='store', help="Path to directory to contain L2 imagery")
     array_id = int(os.getenv("PBS_ARRAYID"))
+    sen_2_cor_home = os.getenv("SEN2COR_HOME")
     args = parser.parse_args()
 
-    pyeo.core.init_log("sen2cor_{}.log".format(array_id))
+    log = pyeo.core.init_log("sen2cor_{}.log".format(array_id))
+
+    new_home = os.path.join((sen_2_cor_home), array_id)
+    os.mkdir(new_home)
+    log.info("Setting home to {}".format(new_home))
+    os.putenv("SEN2COR_HOME", new_home)
 
     file_list = [os.path.join(args.l1_dir, l1_filename) for l1_filename in sorted(os.listdir(args.l1_dir))]
 
