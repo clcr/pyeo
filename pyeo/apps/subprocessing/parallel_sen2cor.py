238bd914249d677793d1f996abb71a3da342294b
hb91 <hb91@le.ac.uk>
2018-10-18 16:13:59 +0100

    Heiko changes

--- pyeo/apps/subprocessing/parallel_sen2cor.py
+++ pyeo/apps/subprocessing/parallel_sen2cor.py
@@ -4,6 +4,17 @@ import pyeo.core
 import argparse
 import shutil
 
+def moveL2(from_path, to_path):
+    '''
+    removes the S2A directory on a Sentinel-2 scene from the L2 directory
+    then copies the S2A directory from L1 to L2
+    and finally removes S2A directory from L1
+    '''
+    if os.path.exists(to_path):
+        shutil.rmtree(to_path)
+    shutil.copytree(from_path, to_path)
+    shutil.rmtree(from_path)
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Parallel sen2cor')
     parser.add_argument('l1_dir', action='store', help="Path to the directory containing L1 imagery")
@@ -25,5 +36,10 @@ if __name__ == "__main__":
     file_list = [os.path.join(args.l1_dir, l1_filename) for l1_filename in sorted(os.listdir(args.l1_dir))]
 
     l2_name = pyeo.core.apply_sen2cor(file_list[array_id], r"/scratch/clcr/shared/Sen2Cor-02.05.05-Linux64/bin/L2A_Process")
-    shutil.move(os.path.join(args.l1_dir, l2_name), args.l2_dir)
+    from_path = os.path.join(args.l1_dir, os.path.basename(l2_name))
+    to_path = os.path.join(args.l2_dir, os.path.basename(l2_name))
+    log.info("l2_name  : {}".format(l2_name))
+    log.info("Moving   : {}".format(from_path))
+    log.info("Moving to: {}".format(to_path))
+    moveL2(from_path, to_path)
 
