0ca968157b915d02e01d6676d8e01dc44693eada
John Roberts <jfr10@le.ac.uk>
2018-10-29 16:40:01 +0000

    Updating rolling composite

--- pyeo/apps/change_detection/rolling_composite_s2_change_detection.py
+++ pyeo/apps/change_detection/rolling_composite_s2_change_detection.py
@@ -60,41 +60,48 @@ if __name__ == "__main__":
     pyeo.create_file_structure(project_root)
     log = pyeo.init_log(log_path)
 
-    l1_image_path = os.path.join(project_root, r"images/L1")
-    l2_image_path = os.path.join(project_root, r"images/L2")
-    planet_image_path = os.path.join(project_root, r"images/planet")
-    merged_image_path = os.path.join(project_root, r"images/merged")
-    stacked_image_path = os.path.join(project_root, r"images/stacked")
-    catagorised_image_path = os.path.join(project_root, r"output/categories")
-    probability_image_path = os.path.join(project_root, r"output/probabilities")
+    l1_image_dir = os.path.join(project_root, r"images/L1")
+    l2_image_dir = os.path.join(project_root, r"images/L2")
+    planet_image_dir = os.path.join(project_root, r"images/planet")
+    merged_image_dir = os.path.join(project_root, r"images/merged")
+    stacked_image_dir = os.path.join(project_root, r"images/stacked")
+    catagorised_image_dir = os.path.join(project_root, r"output/categories")
+    probability_image_dir = os.path.join(project_root, r"output/probabilities")
 
     # Query and download all images since last composite
     if args.do_download or do_all:
         products = pyeo.check_for_s2_data_by_date(aoi_path, start_date, end_date, conf)
         log.info("Downloading")
-        pyeo.download_new_s2_data(products, l1_image_path)
+        pyeo.download_new_s2_data(products, l1_image_dir)
 
     # Atmospheric correction
     if args.do_preprocess or do_all:
         log.info("Applying sen2cor")
-        pyeo.atmospheric_correction(l1_image_path, l2_image_path, sen2cor_path, delete_unprocessed_image=True)
+        pyeo.atmospheric_correction(l1_image_dir, l2_image_dir, sen2cor_path, delete_unprocessed_image=True)
 
     # Aggregating layers into single image
     if args.do_merge or do_all:
         log.info("Aggregating layers")
-        pyeo.aggregate_and_mask_10m_bands(l2_image_path, merged_image_path, cloud_certainty_threshold)
+        pyeo.aggregate_and_mask_10m_bands(l2_image_dir, merged_image_dir, cloud_certainty_threshold)
 
-    # Stack with
-    if args.do_stack or do_all:
-        log.info("Stacking images with composite")
-        pyeo.stack_old_and_new_images(composite_path, stacked_image_path)
+    for image in pyeo.sort_by_timestamp(os.listdir(merged_image_dir), recent_first=False):
+        log.info("Detecting change for {}".format(image))
 
-    # Classify with composite
-    if args.do_classify or do_all:
-        log.info("Classifying with composite")
-        pyeo.classify_directory(stacked_image_path, model_path, catagorised_image_path, probability_image_path)
+        new_image_path = os.path.join(merged_image_dir, image)
 
+        # Stack with composite
+        if args.do_stack or do_all:
+            log.info("Stacking images with composite")
+            new_stack_path = pyeo.stack_old_and_new_images(composite_path, new_image_path, stacked_image_dir)
 
+        # Classify with composite
+        if args.do_classify or do_all:
+            log.info("Classifying with composite")
+            pyeo.classify_image(stacked_image_dir, model_path, catagorised_image_dir, probability_image_dir)
 
+        # Update composite
+        if args.do_update or do_all:
+            log.info("Updating composite")
+            pyeo.composite_images_with_mask((composite_path, new_image_path), composite_path)
 
-
+    log.info("***PROCESSING END***")
