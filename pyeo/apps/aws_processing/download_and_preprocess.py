1dbeb2e65b5f5c81cd8d1e844d305ccc5d9e3f30
John Roberts <jfr10@le.ac.uk>
2018-11-01 11:54:46 +0000

    Added a function to get model, config file and AOI from S3

--- pyeo/apps/aws_processing/download_and_preprocess.py
+++
@@ -1,9 +0,0 @@
-import sys, os
-sys.path.insert(0, os.path.abspath(os.path.join(__file__, '..', '..', '..', '..')))
-import pyeo.core as pyeo
-import argparse
-import configparser
-import boto3
-
-if __name__ == "__main__":
-    parser = configparser.ConfigParser()
