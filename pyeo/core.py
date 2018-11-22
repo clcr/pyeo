6dbc43bf1e06d140ce33bb42b114adc305f6dd70
hb91 <hb91@le.ac.uk>
2018-11-22 20:40:37 +0000

    changed log.

--- pyeo/core.py
+++ pyeo/core.py
@@ -1296,7 +1296,10 @@ def classify_image(image_path, model_path, class_out_dir, prob_out_dir=None,
         for i, prob_val in zip(good_indices, probs):
             prob_out_array[i] = prob_val
         log.info("   Creating GDAL probability image")
-        prob_out_image.GetVirtualMemArray(eAccess=gdal.GF_Write)[:, :, :] = \
+        log.info("   N Classes = {}".format(prob_out_array.shape[1]))
+        log.info("   Image X size = {}".format(image.RasterXSize))
+        log.info("   Image Y size = {}".format(image.RasterYSize))
+        prob_out_image.GetVirmoretualMemArray(eAccess=gdal.GF_Write)[:, :, :] = \
             reshape_prob_out_to_raster(prob_out_array, image.RasterXSize, image.RasterYSize)
 
     class_out_image = None
