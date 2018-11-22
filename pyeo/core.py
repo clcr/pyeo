97eef7bb4bd9c86d732a57f7c9673ff676406b41
hb91 <hb91@le.ac.uk>
2018-11-22 23:24:31 +0000

    trying to find good pixels

--- pyeo/core.py
+++ pyeo/core.py
@@ -1249,7 +1249,7 @@ def classify_image(image_path, model_path, class_out_dir, prob_out_dir=None,
 
     # Determine where in the image array there are no missing values in any of the bands (axis 1)
     log.info("Finding good pixels without missing values")
-    good_samples = [i for i in image_array if np.all(i != nodata, axis = 1)]
+    good_samples = image_array[np.all(image_array != nodata, axis = 1), :]
     good_indices = [i for (i,j) in enumerate(image_array) if np.all(j != nodata, axis = 1)]
     #goodpixels = {i: row for i, row in enumerate(image_array) if not np.any(np.isin(row, nodata))}
     #good_indices = list(goodpixels.keys())
