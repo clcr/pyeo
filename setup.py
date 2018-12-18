49ac7f10d9392f58a14cd2b02140d11cf735ca52
Thingus <jfr10@le.ac.uk>
2018-12-18 22:21:44 +0000

    Attempting to make setup.py work with pip install
    Different sources are giving different formats for setup.py
    Surprising no-one, Python packaging is a mess -_-

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     url='http://pypi.python.org/pypi/Pyeo/',
     license='LICENSE',
     description='Modular processing chain from download to ard',
-    requires=[
+    install_requires=[
         'sentinelsat >= 0.12.1',
         'sentinelhub >= 2.4.2',
         'pytest >= 3.5.0',
