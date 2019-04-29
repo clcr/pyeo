5eb61bdfaac0d40fc220e7504af83b3577b9756c
jfr10 <jfr10@le.ac.uk>
2019-04-29 14:03:47 +0100

    Added show_satellite_image for previewing images

--- pyeo/core.py
+++ pyeo/core.py
@@ -24,6 +24,7 @@ from sklearn.externals import joblib as sklearn_joblib
 import joblib
 import shutil
 import zipfile
+import matplotlib.pyplot as plt
 
 import json
 import csv
@@ -2081,3 +2082,17 @@ def filter_by_class_map(image_path, class_map_path, out_map_path, classes_of_int
 
     log.info("Map filtered")
     return out_map_path
+
+
+def show_satellite_image(image_path):
+    """Uses matplotlib.imshow() to preview a satellite image at image_path. Assumes image is bgr (ie quicklook)"""
+    img = gdal.Open(image_path)
+    array = img.GetVirtualMemArray()
+    if len(array.shape) >= 3:
+        img_view = array.transpose([1,2,0])
+    else:
+        img_view = array
+    plt.imshow(img_view)
+    img_view = None
+    array = None
+    img = None
