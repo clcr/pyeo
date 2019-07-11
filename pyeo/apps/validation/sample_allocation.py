a09c0a7a3c656559d2302ded960a5f81cd81f02e
jfr10 <jfr10@le.ac.uk>
2019-07-11 15:16:19 +0100

    Fixing import

--- pyeo/apps/validation/sample_allocation.py
+++ pyeo/apps/validation/sample_allocation.py
@@ -1,5 +1,6 @@
+import os, sys
+sys.path.insert(0, os.path.abspath(os.path.join(__file__, '..', '..', '..', '..')))
 from pyeo import validation
-from pyeo.core import init_log
 import configparser
 import argparse
 
