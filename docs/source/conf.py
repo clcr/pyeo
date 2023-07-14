b9875099717dd3041fcf4093f53a6cb32cd4accb
Matt Payne <mattpayne@Matts-MacBook-Air.local>
2023-07-14 10:47:57 +0100

    static path debugging

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -94,8 +94,8 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['static']
-html_baseurl = 'https://clcr.github.io/pyeo/'
+html_static_path = ['../build/html/_static']
+# html_baseurl = 'https://clcr.github.io/pyeo/'
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
