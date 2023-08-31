b550e64c13e106317354a465be8d368298eba805
Heiko Balzter <eogit@users.noreply.github.com>
2023-08-31 12:18:48 +0100

    Revert "Revert "Fixed bugs with list[type] declarations and changed in Sentinelhub library with data_request being replaced by aws.""
    This reverts commit b2961c9958de32061899287130f7c784bbe73393.

---
+++ pyeo-env/bin/gdal2tiles.py
@@ -0,0 +1,16 @@
+#!/home/sepal-user/pyeo/pyeo-env/bin/python3
+
+import sys
+# import osgeo_utils.gdal2tiles as a convenience to use as a script
+from osgeo_utils.gdal2tiles import *  # noqa
+from osgeo_utils.gdal2tiles import main
+from osgeo.gdal import deprecation_warn
+
+# Running main() must be protected that way due to use of multiprocessing on Windows:
+# https://docs.python.org/3/library/multiprocessing.html#the-spawn-and-forkserver-start-methods
+if __name__ == '__main__':
+    # Trick inspired from https://stackoverflow.com/questions/45720153/python-multiprocessing-error-attributeerror-module-main-has-no-attribute
+    # and https://bugs.python.org/issue42949
+    __spec__ = None
+    deprecation_warn('gdal2tiles')
+    sys.exit(main(sys.argv))
