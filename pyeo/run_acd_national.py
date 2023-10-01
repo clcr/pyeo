97a6dd40cbd141b9a27dae26bbbe18bca94b71dd
hb91 <hb91@le.ac.uk>
2023-10-01 23:00:02 +0100

    intermediate commit

--- pyeo/run_acd_national.py
+++ pyeo/run_acd_national.py
@@ -37,5 +37,4 @@ if __name__ == "__main__":
     # if run from terminal, __name__ becomes "__main__"
     # sys.argv[0] is the name of the python script, e.g. acd_national.py
 
-    # I.R. Changed as a list was getting passed but a single config_path string is expected by the receiving functions
     acd_national.automatic_change_detection_national(sys.argv[1])
