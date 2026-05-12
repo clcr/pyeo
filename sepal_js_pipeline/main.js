d2223adf94e4facb110db8adb12ed5de034d8141
Matt Payne <matthewjpayne1@gmail.com>
2026-05-12 18:03:34 +0100

    timeseries of change images

--- sepal_js_pipeline/main.js
+++ sepal_js_pipeline/main.js
@@ -16,7 +16,7 @@ const privateKey = JSON.parse(fs.readFileSync(keyPath, "utf8"));
 // run main, wrapped within an authentication call
 ee.data.authenticateViaPrivateKey(privateKey, () => {
     ee.initialize(null, null, () => {
-        console.log("Earth Engine initialised. Starting pipeline...");
+        console.log("1. Earth Engine initialised. Starting pipeline...");
 
         // load user-inputs
         const params = inputs.getParameters();
@@ -24,7 +24,7 @@ ee.data.authenticateViaPrivateKey(privateKey, () => {
         /////////
         // get baseline mosaic
         /////////
-        console.log("Generating baseline mosaic...")
+        console.log("2. Generating baseline mosaic...")
         const baseline = imagery.getBaselineMosaic(
             params.roi,
             params.baseline.start,
@@ -47,13 +47,13 @@ ee.data.authenticateViaPrivateKey(privateKey, () => {
             dimensions: 800
         };
         baseline.getThumbURL(visParams, (url) => {
-            console.log("\n Quicklook:", url);
+            console.log("\n Baseline Mosaic Quicklook:", url);
         });
 
         /////////
         // create a classifier on the baseline
         /////////
-        console.log("Training a Classifier...");
+        console.log("3. Training a Classifier...");
         const modelResults = classification.trainGBClassifier({
             baselineImage: baseline,
             trainingPoints: params.trainingFeatures,
@@ -67,6 +67,52 @@ ee.data.authenticateViaPrivateKey(privateKey, () => {
         modelResults.validationAccuracy.evaluate((acc) => {
             console.log(`Validation accuracy: ${(acc * 100).toFixed(2)}%`);
         });
+
+        /////////
+        // apply the classifier on the baseline
+        /////////
+        console.log("4. Applying the Classifier on the baseline...");
+        const classifiedBaseline = classification.classifyImage({
+            image: baseline,
+            trainedClassifier: modelResults.classifier
+        })
+
+        // get the parameters for the classification quicklook
+        const classValues = Object.values(params.classLabels);
+        const maxClassValue = Math.max(...classValues);
+        const dynamicPalette = Object.keys(params.classLabels)
+            .sort((a, b) => params.classLabels[a] - params.classLabels[b])
+            .map(key => params.classColours[key]);
+
+        // quicklook of the classified baseline
+        const classifiedVisParams = {
+            bands: ["classification"],
+            min: 0,
+            max: maxClassValue,
+            palette: dynamicPalette,
+            dimensions: 800
+        };
+        classifiedBaseline.getThumbURL(classifiedVisParams, (url) => {
+            console.log("\n Classified Baseline Quicklook:", url);
+        });
+
+        /////////
+        // get a cloud masked change timeseries
+        /////////
+        console.log("5. Getting a Timeseries of Change Images...");
+        const timeSeries = imagery.getChangeTimeSeries({
+            roi: params.roi,
+            startDate: params.change.start,
+            endDate: params.change.end,
+            cloudCoverThreshold: params.cloudCoverThreshold,
+            bandsOfInterest: params.bandsOfInterest
+        })
+
+        /////////
+        // apply the classifier on the change timeseries
+        /////////
+        // console.log("6. Applying the Classifier on the Timeseries...");
+        // const classifiedTimeseries = classification
         
     }, (e) => console.error("Initialisation error: ", e));
 }, (e) => console.error("Authentication error: ", e));
