8136406649b2e1a952d899f8b2bc1007b067c869
Matt Payne <matthewjpayne1@gmail.com>
2026-05-12 10:40:47 +0100

    added bandsOfInterest

--- sepal_js_pipeline/config/inputs.js
+++ sepal_js_pipeline/config/inputs.js
@@ -23,6 +23,8 @@ const getParameters = () => {
             end: "2026-05-08"
         },
 
+        bandsOfInterest: ['B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'B8A', 'B11', 'B12'],
+
         cloudCoverThreshold: 25,
 
         changeFromClass: 1,
