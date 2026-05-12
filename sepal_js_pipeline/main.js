9f1bb7779fe6d6bb4ca3f55b61c709a441b31075
Matt Payne <matthewjpayne1@gmail.com>
2026-05-12 11:05:41 +0100

    band selection, creating classifier, model performance

--- sepal_js_pipeline/main.js
+++ sepal_js_pipeline/main.js
@@ -6,8 +6,8 @@ const ee = require("@google/earthengine");
 // import custom modules
 const inputs = require("./config/inputs");
 const imagery = require("./src/imagery");
-// classification module would go here
-// change detection module would go here
+const classification = require("./src/classification");
+// change detection module require goes here
 
 // auth
 const keyPath = process.env.EE_PRIVATE_KEY_PATH;
@@ -21,15 +21,22 @@ ee.data.authenticateViaPrivateKey(privateKey, () => {
         // load user-inputs
         const params = inputs.getParameters();
 
+        /////////
         // get baseline mosaic
+        /////////
         console.log("Generating baseline mosaic...")
         const baseline = imagery.getBaselineMosaic(
             params.roi,
             params.baseline.start,
             params.baseline.end,
-            params.cloudCoverThreshold
+            params.cloudCoverThreshold,
+            params.bandsOfInterest
         );
-        //console.log("Baseline band names : ", baseline.bandNames().getInfo());
+
+        baseline.bandNames().evaluate((bands, error) => {
+            if (error) console.error('Band error:', error);
+            else console.log('Baseline image bands:', bands);
+        });
 
         // quicklook of the baseline
         const visParams = {
@@ -41,6 +48,24 @@ ee.data.authenticateViaPrivateKey(privateKey, () => {
         };
         baseline.getThumbURL(visParams, (url) => {
             console.log("\n Quicklook:", url);
+        });
+
+        /////////
+        // create a classifier on the baseline
+        /////////
+        console.log("Training a Classifier...");
+        const modelResults = classification.trainGBClassifier({
+            baselineImage: baseline,
+            trainingPoints: params.trainingFeatures,
+            classProperty: "class"
+        });
+
+        // use evaluate to get model performance information
+        modelResults.trainingAccuracy.evaluate((acc) => {
+            console.log(`Training accuracy: ${(acc * 100).toFixed(2)}%`);
+        });
+        modelResults.validationAccuracy.evaluate((acc) => {
+            console.log(`Validation accuracy: ${(acc * 100).toFixed(2)}%`);
         });
         
     }, (e) => console.error("Initialisation error: ", e));
